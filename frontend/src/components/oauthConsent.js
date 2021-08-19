e40e8680fdf1e9e4f9530838d25cb2250f5aec4e
kmabadshah <kmabadshah@gmail.com>
2021-08-19 09:22:19 +0600

    removed duplicate file

--- frontend/src/components/oauthConsent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OauthConsent() {
-    React.useLayoutEffect(() => { window.localstorage.setItem('oauth_url', window.location.href)
-        window.close()
-    }, [])
-
-    return null;
-}
