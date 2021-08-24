fb7bccdee2f95f6e91fe81f62602f14677cb6425
kmabadshah <kmabadshah@gmail.com>
2021-08-24 11:31:24 +0600

    excluded backend-django

--- backend-django/backend/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['user_id', 'username', 'image_url', 'profile_description']
